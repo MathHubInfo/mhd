b303862b78ed6ecca9dfab80f6eab6fb09391eec
Tom Wiesing <tkw01536@gmail.com>
2019-06-24 15:38:13 +0200

    Example generated files

---
+++ mdh/generated/y.py
@@ -0,0 +1,12 @@
+from django.db import models
+from mdh import magic
+
+class Y(models.Model, magic.MDHModel):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+__all__ = ['Y']
