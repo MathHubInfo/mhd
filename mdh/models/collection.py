39ce729fb0465b6d4435d9f12ec5bb10d0b28b2c
Tom Wiesing <tkw01536@gmail.com>
2019-07-27 23:41:58 +0200

    Generalize models with metadata
    This commit overwrites previous migrations and adds a new generic
    handling for models with metadata field.

--- mdh/models/collection.py
+++ mdh/models/collection.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-class Collection(models.Model):
+from .utils import ModelWithMetadata
+
+
+class Collection(ModelWithMetadata):
     """ Collection of Mathmatical Items """
     display_name = models.TextField(help_text="Name of this collection")
     slug = models.SlugField(help_text="Identifier of this collection", unique=True)
-
-    metadata = models.TextField(null=True, blank=True, help_text="Metadata for this collection")
 
 __all__ = ["Collection"]
