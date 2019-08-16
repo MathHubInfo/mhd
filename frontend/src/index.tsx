810a674eec7dbf3a9f358a485484a33995c0e8bd
Tom Wiesing <tkw01536@gmail.com>
2019-08-16 21:05:45 +0200

    frontend: Use fontawesome from npm

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// load font-awesome
+require('@fortawesome/fontawesome-free/js/all.js');
+
 ReactDOM.render(<BrowserRouter>
     <App results_loading_delay={200} />
 </BrowserRouter>, document.getElementById('app'));
