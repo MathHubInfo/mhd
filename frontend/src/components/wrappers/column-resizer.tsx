3f453962ea86a63b5ba6c2c7224c1ddc7419dee1
Tom Wiesing <tkw01536@gmail.com>
2019-10-01 16:58:33 +0200

    Add back padding
    This commit adds back specific hard-codded paddding for the style to
    work.

--- frontend/src/components/wrappers/column-resizer.tsx
+++ frontend/src/components/wrappers/column-resizer.tsx
@@ -17,6 +17,7 @@ export default class ColumnResizer extends ColumnResizerOrg {
         this.fixStyles();
     }
 
+    // hash of the old style that is overwritten
     private static OVERWRITE_HASH = stringHash(
         ' .grip-resizable{table-layout:fixed;} .grip-resizable > tbody > tr > td, .grip-resizable > tbody > tr > th{overflow:hidden}'
     + ' .grip-padding > tbody > tr > td, .grip-padding > tbody > tr > th{padding-left:0!important; padding-right:0!important;}'
@@ -24,8 +25,10 @@ export default class ColumnResizer extends ColumnResizerOrg {
     + ' .grip-handle .grip-resizable{position:absolute;background-color:red;filter:alpha(opacity=1);opacity:0;width:10px;height:100%;cursor: col-resize;top:0px}'
     + ' .grip-lastgrip{position:absolute; width:1px; } .grip-drag{ border-left:1px dotted black;	}'
     + ' .grip-flex{width:auto!important;} .grip-handle.grip-disabledgrip .grip-resizable{cursor:default; display:none;}').toString();
-
+    
+    // the new replaced style that is being set
     private static NEW_STYLE = ' .grip-resizable{table-layout:fixed;} .grip-resizable > tbody > tr > td, .grip-resizable > tbody > tr > th{overflow:hidden}'
+    + ' .grip-padding > tbody > tr > td, .grip-padding > tbody > tr > th{padding-left:5px!important; padding-right:5px!important;}'
     + ' .grip-container{ height:0px; position:relative;} .grip-handle{margin-left:-5px; position:absolute; z-index:5; }'
     + ' .grip-handle .grip-resizable{position:absolute;background-color:red;filter:alpha(opacity=1);opacity:0;width:10px;height:100%;cursor: col-resize;top:0px}'
     + ' .grip-lastgrip{position:absolute; width:1px; } .grip-drag{ border-left:1px dotted black;	}'
