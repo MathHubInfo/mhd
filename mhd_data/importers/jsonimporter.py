3867448691bb56e3ee151d47b0488bc727af7538
Tom Wiesing <tkw01536@gmail.com>
2019-10-16 13:52:27 +0200

    Add internal chunks to JSONFileImporter
    This commit adds internal chunks of size 1000000 to JSONFileImporter to
    allow importing large amounts of data at once.

--- mhd_data/importers/jsonimporter.py
+++ mhd_data/importers/jsonimporter.py
@@ -9,10 +9,11 @@ from mhd_provenance.models import Provenance
 class JSONFileImporter(DataImporter):
     """ An importer that loads data from a set of json files """
 
-    def __init__(self, collection_slug, property_names, data_path, provenance_path, quiet, batch_size):
-        collection = Collection.objects.get(slug=collection_slug)
-        properties = [collection.get_property(pn) for pn in property_names]
+    def __init__(self, collection_slug, property_names, data_path, provenance_path, quiet, batch_size, inner_chunk_size = 1000000):
+        # inner chunk size
+        self._inner_chunk_size = inner_chunk_size
 
+        # file list
         self._files = deque([])
         if isinstance(data_path, list) or isinstance(data_path, tuple):
             for d in data_path:
@@ -20,8 +21,19 @@ class JSONFileImporter(DataImporter):
         else:
             self._files.append(data_path)
 
-
+        # path to provenance
         self.provenance_path = provenance_path
+
+
+        # buffer for current file chunk
+        self._chunk = None
+        self._chunk_fn = None
+        self._chunk_offset = None
+
+
+        # call super()
+        collection = Collection.objects.get(slug=collection_slug)
+        properties = [collection.get_property(pn) for pn in property_names]
         super().__init__(collection, properties, quiet, batch_size)
 
     def create_provenance(self):
@@ -40,12 +52,44 @@ class JSONFileImporter(DataImporter):
             Should return None if no more chunks are left.
         """
 
+        # grab the next chunk (if we have nothing left)
+        if self._chunk is None or len(self._chunk) == 0:
+            self._chunk = self._get_next_chunk()
+
+        # if there are no chunks left, return
+        if self._chunk is None:
+            return None
+
+        # get meta data for the current chunk
+        meta = {
+            'filename': self._chunk_fn,
+            'offset': self._chunk_offset
+        }
+
+        # cut the chunk to at most inner_chunk_size
+        if len(self._chunk) > self._inner_chunk_size:
+            c = self._chunk[:self._inner_chunk_size]
+            self._chunk_offset += self._inner_chunk_size
+            self._chunk = self._chunk[self._inner_chunk_size:]
+        else:
+            c = self._chunk
+            self._chunk = None
+
+        return {'data': c, 'meta': meta}
+
+    def _get_next_chunk(self):
+        """
+            Loads the next file representing a chunk from disk.
+        """
+
         # if we have no files left, bail
         if len(self._files) == 0:
             return None
 
-        # get the next file path
+        # get the next file path (and store the file path)
         data_path = self._files.popleft()
+        self._chunk_fn = data_path
+        self._chunk_offset = 0
 
         # read all of the data file
         with open(data_path) as f:
@@ -68,4 +112,13 @@ class JSONFileImporter(DataImporter):
             To be implemented by the subclass.
         """
 
-        return [r[idx] for r in chunk]
+        return [r[idx] for r in chunk['data']]
+
+    def get_chunk_length(self, chunk):
+        """
+            Gets the length of the given chunk.
+            By default¸ simply calls len() on the chunk object,
+            but this may be overwritten by the subclass.
+        """
+
+        return len(chunk['data'])
