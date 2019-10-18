13258c097a8a897f305ef6dc8341d4a512a137c1
Tom Wiesing <tkw01536@gmail.com>
2019-10-18 13:37:15 +0200

    Move Provenance to UUID

--- mhd_provenance/models.py
+++ mhd_provenance/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-from mhd.utils import ModelWithMetadata
+from mhd.utils import ModelWithMetadata, uuid4
 
 
 class Provenance(ModelWithMetadata):
     """ The Provenance model represents provenance-data for a single cell """
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
 
     time = models.DateTimeField(auto_now_add=True, help_text="Time at which this provenance was created")
     predecessor = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, help_text="Previous provenance")
