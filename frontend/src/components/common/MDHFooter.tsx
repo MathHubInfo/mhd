9040ca73f6873dd7a8f7484e488b930bd44414bb
Tom Wiesing <tkw01536@gmail.com>
2019-10-22 17:17:27 +0200

    Add logos

--- frontend/src/components/common/MDHFooter.tsx
+++ frontend/src/components/common/MDHFooter.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
+import eu_logo from '../../images/logos/eu.svg';
+import fau_logo from '../../images/logos/fau_logo.png';
+import kwarc_logo from '../../images/logos/kwarc_logo.png';
+import odk_logo from '../../images/logos/odk_logo.png';
+
 /** Footer for the entire website */
 export default function MDHFooter() {
-    return(
-        <footer className="bg-dark text-white">
+    return (
+        <footer>
             <Container>
-                <Row>
-                    <Col lg="4" className="my-5">
-                    </Col>
-                    <Col lg="4" className="my-5">
-                    </Col>
-                    <Col lg="4" className="my-5">
+                <Row style={{textAlign: 'center'}}>
+                    <Col>
+                        <LogoLink url="https://kwarc.info/" pic={kwarc_logo} alt="KWARC research group" width={80} height={80} />
+                        <LogoLink url="https://fau.de/" pic={fau_logo} alt="FAU Erlangen-Nürnberg" width={270} height={53} />
+                        <LogoLink url="https://opendreamkit.org/" pic={odk_logo} alt="OpenDreamKit" width={45} height={69} />
+                        <LogoLink url="https://europa.eu/" pic={eu_logo} alt="EU" width={120} height={80} />
                     </Col>
                 </Row>
             </Container>
         </footer>
+    );
+}
+
+function LogoLink(props: { url: string; pic: any; alt: string, width: number, height: number }) {
+    return (
+        <a
+            href={props.url}
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={props.pic} alt={props.alt} style={{ width: props.width, height: props.height }} />
+            &nbsp;
+        </a>
     );
 }
