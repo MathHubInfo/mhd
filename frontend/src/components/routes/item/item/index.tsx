49a6afd796579b1a5d9b0f9607a8af1766e3385b
Tom Wiesing <tkw01536@gmail.com>
2019-10-23 15:28:45 +0200

    Add a better item page

--- frontend/src/components/routes/item/item/index.tsx
+++ frontend/src/components/routes/item/item/index.tsx
@@ -2,26 +2,51 @@ import React from 'react';
 import MDHMain from "../../../common/MDHMain";
 import { ParsedMDHCollection } from "../../../../client/derived";
 import { TMDHItem } from "../../../../client/rest";
+import { Table, Row, Col, Container } from "reactstrap";
 
 interface MDHItemViewProps {
     /** collection */
     collection: ParsedMDHCollection;
     
     /** item */
-    item: TMDHItem<{}>
+    item: TMDHItem<any>
 }
 
 /** Renders a collection that is not found */
 export default class MDHItemView extends React.Component<MDHItemViewProps> {
     render() {
-        const { item } = this.props;
+        const { collection, item } = this.props;
+        
+        // render rows for the main table
+        const rows = collection.properties.map(p => {
+            const codec = collection.codecMap.get(p.slug);
+            if (!codec) return null;
 
-        const leftHead = JSON.stringify(item);
-        /*const buttons = <>
-            <Link to={`/collection/${collection.slug}/`}><Button>Back to Collection</Button></Link>
-        </>;*/
-        const buttons = null;
+            const Cell = codec.cellComponent;
+            return <tr key={p.slug}>
+                <td>{p.displayName}</td>
+                <td><Cell value={item[p.slug]} codec={codec} /></td>
+            </tr>
+        });
 
-        return <MDHMain title={`Item ${item._id}`} leftHead={leftHead} buttons={buttons} />;
+        return <MDHMain title={`Item ${item._id}`}>
+            <Container>
+                <Row>
+                    <Col sm="12">
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th>Property</th>
+                                    <th>Value</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {rows}
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
+        </MDHMain>;
     }
 }
