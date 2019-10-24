f82cdba09c2d6af11c596e9e31136ddf3c2507c4
Tom Wiesing <tkw01536@gmail.com>
2019-10-24 09:08:09 +0200

    'delete_collection' + 'flush_collection' commands
    This commit adds two new manage.py commands 'delete_collection' and
    'flush_collection' respectively. This removes all items from a
    collection and deletes it.

---
+++ mhd_data/management/commands/flush_collection.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from ...models import Collection
+
+
+class Command(BaseCommand):
+    help = 'Removes all items from a collection and deletes associated values from the database'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'slug', help="Slug of collection to flush")
+        parser.add_argument('--simulate', '-s', action="store_true",
+                    help="Only simulates flushing")
+
+    @with_simulate_arg
+    def handle(self, *args, **kwargs):
+        collection = Collection.objects.get(slug=kwargs['slug'])
+        collection.flush()
---
+++ mhd_schema/management/commands/delete_collection.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from ...models import Collection
+
+
+class Command(BaseCommand):
+    help = 'Deletes a collection. '
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'slug', help="Slug of collection to delete")
+        parser.add_argument('--simulate', '-s', action="store_true",
+                    help="Only simulate deletion")
+
+    @with_simulate_arg
+    def handle(self, *args, **kwargs):
+        collection = Collection.objects.get(slug=kwargs['slug'])
+        collection.safe_delete()
