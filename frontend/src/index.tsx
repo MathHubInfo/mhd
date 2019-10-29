d7be245857251fbb6adc7be2322485710803bf31
Tom Wiesing <tkw01536@gmail.com>
2019-10-29 15:47:05 +0100

    Add serviceWorker dummy

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+
 
 // load the required fonts
 require('typeface-cormorant-garamond');
@@ -18,3 +20,8 @@ require('@fortawesome/fontawesome-free/js/all.js');
 
 // and render the app
 ReactDOM.render(<BrowserRouter><App results_loading_delay={200} /></BrowserRouter>, document.getElementById('app'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
