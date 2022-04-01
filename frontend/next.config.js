9d33034d26a11da9266a466f61de103b24740e74
Tom Wiesing <tkw01536@gmail.com>
2022-04-01 16:19:55 +0200

    frontend: Proxy static files properly

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: "/api/admin/static/:path*",
+        destination: `${process.env.DJANGO_URL}/api/admin/static/:path*`,
+      },
+      {
         source: "/api/:path*",
         destination: `${process.env.DJANGO_URL}/api/:path*/`,
       },
