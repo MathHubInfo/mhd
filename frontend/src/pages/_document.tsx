8a25909847f50e3425c3c00b5bc52e12a249f167
Tom Wiesing <tkw01536@gmail.com>
2022-04-05 01:24:52 +0200

    frontend: Setup 'react-beautiful-dnd' SSR properly
    When switching to NextJS, any usage of 'react-beautiful-dnd' was broken.
    This was caused because previous usage did not involve SSR.
    
    This commit adds a custom _document.tsx that calls the
    'resetServerContext()' function in accordance with appropriate
    documentation.

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import { resetServerContext } from "react-beautiful-dnd";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    // Support react-beautiful-dnd SSR properly
+    // See https://github.com/atlassian/react-beautiful-dnd/blob/67b96c8d04f64af6b63ae1315f74fc02b5db032b/docs/api/reset-server-context.md
+    resetServerContext();
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
