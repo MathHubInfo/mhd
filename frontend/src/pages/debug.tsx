76f038ffbb380bbeaf7dfa4e4c378297ae9544ee
Tom Wiesing <tkw01536@gmail.com>
2022-04-07 12:45:08 +0200

    frontend/debug: Hide in production

--- frontend/src/pages/debug.tsx
+++ frontend/src/pages/debug.tsx
@@ -1,15 +1,16 @@
+import { GetStaticProps } from "next";
 import React from "react";
 import Table, { CellComponentProps, TableColumn, TableState } from "../components/wrappers/table";
 
 // TODO: Consider making this page debug only
 
-class Header extends React.Component<{column: TableColumn<any>}> {
+class Header extends React.Component<{ column: TableColumn<any> }> {
     render() {
         return this.props.column.key;
     }
 }
 
-class Cell extends React.Component<{data: any}> {
+class Cell extends React.Component<{ data: any }> {
     render() {
         return JSON.stringify(this.props.data);
     }
@@ -30,26 +31,26 @@ export default class DebugComponent extends React.Component<{}, DebugTableState>
         },
         columns: DebugComponent.columns.slice(0),
     }
-    
+
     private onTableStateChange = (newState: TableState) => {
-        this.setState({table: newState});
+        this.setState({ table: newState });
     }
 
     private static columns: TableColumn<any>[] = [
         {
             key: "x",
             Header,
-            Cell: ({data}: CellComponentProps<any>) => <Cell data={data[0]} />,
+            Cell: ({ data }: CellComponentProps<any>) => <Cell data={data[0]} />,
         },
         {
             key: "2x",
             Header,
-            Cell: ({data}: CellComponentProps<any>) => <Cell data={data[1]} />,
+            Cell: ({ data }: CellComponentProps<any>) => <Cell data={data[1]} />,
         },
         {
             key: "3x",
             Header,
-            Cell: ({data}: CellComponentProps<any>) => <Cell data={data[2]} />,
+            Cell: ({ data }: CellComponentProps<any>) => <Cell data={data[2]} />,
         },
     ]
 
@@ -57,7 +58,7 @@ export default class DebugComponent extends React.Component<{}, DebugTableState>
     private static generateData(page: number, page_size: number): number[][] {
         // generate page_size number of xs and offset them appropriatly
         const xs = Array.from(Array(page_size).keys()).map(x => x + page * page_size).filter(x => x < this.MAX_DATA);
-        return xs.map(x => [x, 2*x, 3*x]);
+        return xs.map(x => [x, 2 * x, 3 * x]);
     }
 
 
@@ -70,22 +71,22 @@ export default class DebugComponent extends React.Component<{}, DebugTableState>
 
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
-      
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
-      
-          // And swap it with the current element.
-          temporaryValue = columns[currentIndex];
-          columns[currentIndex] = columns[randomIndex];
-          columns[randomIndex] = temporaryValue;
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = columns[currentIndex];
+            columns[currentIndex] = columns[randomIndex];
+            columns[randomIndex] = temporaryValue;
         }
-      
+
         this.setState({ columns });
     }
 
     render() {
-        const {table, columns} = this.state;
+        const { table, columns } = this.state;
         const data = DebugComponent.generateData(table.page, table.per_page);
         const total_pages = Math.ceil(DebugComponent.MAX_DATA / table.per_page);
 
@@ -101,4 +102,9 @@ export default class DebugComponent extends React.Component<{}, DebugTableState>
             <button onClick={this.shuffleColumns}>Shuffle Columns</button>
         </>;
     }
+}
+
+export const getStaticProps: GetStaticProps = async function (context) {
+    if (process.env.NODE_ENV !== "development") return { notFound: true };
+    return { props: {} }
 }
