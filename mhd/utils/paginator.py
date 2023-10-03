d46b77d486c2425eff9bce23ffa6d0cac9d96f11
Tom Wiesing <tkw01536@gmail.com>
2023-10-03 16:45:44 +0200

    Relative pagination links

--- mhd/utils/paginator.py
+++ mhd/utils/paginator.py
@@ -12,9 +12,36 @@ from typing import TYPE_CHECKING, Any
 if TYPE_CHECKING:
     pass
 
+def after(s: str, needle: str):
+    try:
+        idx = s.index(needle)
+        return s[idx+len(needle):], True
+    except ValueError:
+        return s, False
+
 class DefaultPaginator(pagination.PageNumberPagination):
     page_size_query_param: str = "per_page"
     max_page_size: int = 1000
+
+    def clean_pagination_link(self, link):
+        if not isinstance(link, str):
+            return link
+        
+        link, ok = after(link, '://')
+        if not ok:
+            return link
+
+        link, ok = after(link, '/')
+        if not ok:
+            return link
+        return '/' + link
+
+
+    def get_next_link(self):
+        return self.clean_pagination_link(super().get_next_link())
+    
+    def get_prev_link(self):
+        return self.clean_pagination_link(super().get_prev_link())
 
     def get_paginated_response(self, data: list[Any]) -> Response:
         return Response(OrderedDict([
